1097839d81951e61e8e3018777d2594805429366
Avner <aluvton@gmail.com>
2026-06-14 20:45:57 +0300

    Add quick and deep job scan commands

---
+++ server/src/cliDeepScan.js
@@ -0,0 +1,19 @@
+﻿import "dotenv/config";
+
+process.env.SEARCH_PROVIDERS = "alljobs,drushim,jobmaster";
+process.env.SEARCH_PROVIDER = "alljobs,drushim,jobmaster";
+
+process.env.SCAN_MAX_QUERIES = "8";
+process.env.SCAN_BATCH_SIZE = "6";
+
+process.env.ALLJOBS_MAX_PAGES = "8";
+process.env.ALLJOBS_MAX_RESULTS = "180";
+process.env.ALLJOBS_FETCH_DETAILS = "true";
+process.env.ALLJOBS_DETAIL_LIMIT = "80";
+process.env.ALLJOBS_DETAIL_DELAY_MS = "100";
+
+process.env.DRUSHIM_MAX_RESULTS = "60";
+process.env.JOBMASTER_MAX_RESULTS = "60";
+
+console.log("Running DEEP job scan...");
+await import("./cliFindJobs.js");
---
+++ server/src/cliQuickScan.js
@@ -0,0 +1,18 @@
+﻿import "dotenv/config";
+
+process.env.SEARCH_PROVIDERS = "alljobs,drushim";
+process.env.SEARCH_PROVIDER = "alljobs,drushim";
+
+process.env.SCAN_MAX_QUERIES = "2";
+process.env.SCAN_BATCH_SIZE = "4";
+
+process.env.ALLJOBS_MAX_PAGES = "3";
+process.env.ALLJOBS_MAX_RESULTS = "60";
+process.env.ALLJOBS_FETCH_DETAILS = "true";
+process.env.ALLJOBS_DETAIL_LIMIT = "20";
+process.env.ALLJOBS_DETAIL_DELAY_MS = "100";
+
+process.env.DRUSHIM_MAX_RESULTS = "30";
+
+console.log("Running QUICK job scan...");
+await import("./cliFindJobs.js");
